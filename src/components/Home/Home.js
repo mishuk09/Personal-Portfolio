2ecd92b1b5365cc49029d76e1e8e55c0945a69ad
mishuk09 <mishukinfo09@gmail.com>
2023-11-15 20:34:07 +0530

    add sort introduction

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,43 +1,77 @@
 import React from 'react';
 import './Home.css'
-import { faGithub, faFacebook, faLinkedin, faTwitch, } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faFacebook, faLinkedin, faTwitch, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import home from './img/home.png'
+import wave from './img/wave.svg';
+import sortintro from './img/sortintro.png'
+import linkedinfollow from './img/linkedinfollow.png'
 
 import homelogo from './img/homelogo.png';
 
 const Home = () => {
     return (
-        <div className='container home-div'>
+        <div>
+            <div className='container   home-div'>
 
-            <div className='name-div'>
+                {/* whatsapp logo */}
+                <div className='whatsapp-logo'><FontAwesomeIcon icon={faWhatsapp} size="2x" beat /></div>
+                <div className='name-div'>
 
-                <p className='name-p'>Welcome to my Portfolio</p>
-                <div className='name-div22'>
-                    <p>I'm</p>
-                    <h1>MAHADI HASAN <span>MISHUK</span> </h1>
+                    <p className='name-p'>Welcome to my Portfolio</p>
+                    <div className='name-div22'>
+                        <p>I'm</p>
+                        <h1>MAHADI HASAN <span>MISHUK</span> </h1>
+                        <hr />
+                    </div>
+                    {/* <p>
+                        I'm{' '}
+                        <Typed
+                            strings={['Designer', 'Developer', 'Freelancer', 'Photographer']}
+                            typeSpeed={50}
+                            backSpeed={25}
+                            backDelay={1500}
+                            loop
+                        />
+                    </p> */}
+                    {/* <p>Junior web Developer</p> */}
+                    {/* <p>I'm <span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p> */}
+                    <p  >I am Sincare,responsible,confident,dedicated,down to earth & trustworthly.Currently I am studying about Web-development as well as Softwer Engineering.</p>
+
+                    <div className=' icon-div mt-10'>
+                        <div> <img src={homelogo} alt="" /></div>
+                        <div className='social-link pt-8 '>
+
+                            <div className='ps-4 h-5 w-'><FontAwesomeIcon icon={faGithub} size="2x" />  </div>
+                            <div className='ps-4 h-5 w-'><FontAwesomeIcon icon={faLinkedin} size="2x" /></div>
+                            <div className='ps-4 h-5 w-'><FontAwesomeIcon icon={faFacebook} size="2x" /></div>
+                            <div className='ps-4 h-5 w-'><FontAwesomeIcon icon={faTwitch} size="2x" /></div>
+                        </div>
+                    </div>
                 </div>
-                <p>Junior web Developer</p>
-                <p  >I am Sincare,responsible,confident,dedicated,down to earth & trustworthly.Currently I am studying about Web-development as well as Softwer Engineering.</p>
 
-                <div className=' icon-div'>
-                    <div> <img src={homelogo} alt="" /></div>
-                    <div className='social-link  '>
-                        {/* <div><FontAwesomeIcon icon={faHome} />  </div> */}
-                        <div className='m-10'><FontAwesomeIcon icon={faGithub} />  </div>
-                        <div><FontAwesomeIcon icon={faLinkedin} /></div>
-                        <div><FontAwesomeIcon icon={faFacebook} /></div>
-                        <div><FontAwesomeIcon icon={faTwitch} /></div>
+
+                <div className='pic-div'>
+                    <img className='container-fluid' src={home} alt="" />
+                </div>
+            </div>
+
+            <div className='container container-fluid wave1'>
+                <img className='' src={wave} alt="" />
+
+                <div className='sort-intro'>
+                    <div className='sort-intro-headline'>
+                        <div><img src={sortintro} alt="" /></div>
+                        <div></div>
+                        <div><img src={linkedinfollow} alt="" /></div>
+                    </div>
+                    <div>
+                        <div></div>
+                        <div></div>
                     </div>
                 </div>
             </div>
 
-
-            <div className='pic-div'>
-                <h1 className=" text-sm text-gray-500 ">
-                    Hello world!
-                </h1>
-            </div>
         </div>
     );
 };
